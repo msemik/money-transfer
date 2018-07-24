fe3a90de3d5f24b90edaa5d65323f381394928ef
michal <michal.semik@gmail.com>
2018-07-24 16:27:03 +0200

    pageable

--- src/main/java/com/baseinterview/baseinterview/sample/customer/CustomerController.java
+++ src/main/java/com/baseinterview/baseinterview/sample/customer/CustomerController.java
@@ -1,6 +1,9 @@
 package com.baseinterview.baseinterview.sample.customer;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -9,6 +12,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("sample/customer")
+@Slf4j
 public class CustomerController {
     private final CustomerRepository repository;
 
@@ -18,21 +22,28 @@ public class CustomerController {
     }
 
     @PostMapping
-    public Customer create(@RequestBody @Valid Customer input){
+    public Customer create(@RequestBody @Valid Customer input) {
         return repository.save(input);
     }
 
     @GetMapping
-    public List<Customer> findAll(){
+    public List<Customer> findAll() {
         return repository.findAll();
     }
 
     @GetMapping("{id}")
-    public Optional<Customer> findById(@PathVariable("id") Long id){
+    public Optional<Customer> findById(@PathVariable("id") Long id) {
         return repository.findById(id);
     }
+
     @GetMapping("byParam")
-    public Optional<Customer> findByName(@RequestParam("name") String name){
+    public Optional<Customer> findByName(@RequestParam("name") String name) {
         return repository.findByName(name);
+    }
+
+    @GetMapping("pageable")
+    public Page<Customer> findAdultsPageable(Pageable pageable) {
+        log.debug(pageable.toString());
+        return repository.findByAgeIsGreaterThanEqual(18L, pageable);
     }
 }
--- src/main/java/com/baseinterview/baseinterview/sample/customer/CustomerRepository.java
+++ src/main/java/com/baseinterview/baseinterview/sample/customer/CustomerRepository.java
@@ -1,10 +1,15 @@
 package com.baseinterview.baseinterview.sample.customer;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.Optional;
 
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
+public interface CustomerRepository extends JpaRepository<Customer, Long>, PagingAndSortingRepository<Customer, Long> {
 
     Optional<Customer> findByName(String name);
+
+    Page<Customer> findByAgeIsGreaterThanEqual(Long age, Pageable pageable);
 }

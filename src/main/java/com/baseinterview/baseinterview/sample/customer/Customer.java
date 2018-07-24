c7ec7968df339d2e28a23e90a98814b80ca7de99
michal <michal.semik@gmail.com>
2018-07-24 19:35:21 +0200

    creation & update timestamps

--- src/main/java/com/baseinterview/baseinterview/sample/customer/Customer.java
+++ src/main/java/com/baseinterview/baseinterview/sample/customer/Customer.java
@@ -2,11 +2,12 @@ package com.baseinterview.baseinterview.sample.customer;
 
 import lombok.Builder;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
 
 @Entity
 @Data
@@ -20,4 +21,10 @@ public class Customer {
     private String name;
 
     private Long age;
+
+    @CreationTimestamp
+    private LocalDateTime createdDate;
+
+    @UpdateTimestamp
+    private LocalDateTime modifiedDate;
 }

5c1a2d00ee0d3b713d5944b76e434f6873c1f5ea
michal <michal.semik@gmail.com>
2018-07-24 12:52:36 +0200

    add missing @RequestMapping to sample customer

--- src/main/java/com/baseinterview/baseinterview/sample/customer/CustomerController.java
+++ src/main/java/com/baseinterview/baseinterview/sample/customer/CustomerController.java
@@ -1,15 +1,13 @@
 package com.baseinterview.baseinterview.sample.customer;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
 @RestController
+@RequestMapping("sample/customer")
 public class CustomerController {
     private final CustomerRepository repository;
 

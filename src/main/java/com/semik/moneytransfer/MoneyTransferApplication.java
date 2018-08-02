86470809c9b0a7d23ef1b32b563be9651fc66bca
msemik <michal.semik@gmail.com>
2018-08-02 18:34:46 +0200

    allow cors

--- src/main/java/com/semik/moneytransfer/MoneyTransferApplication.java
+++ src/main/java/com/semik/moneytransfer/MoneyTransferApplication.java
@@ -16,6 +16,11 @@ import io.dropwizard.hibernate.HibernateBundle;
 import io.dropwizard.jersey.setup.JerseyEnvironment;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import java.util.EnumSet;
 
 public class MoneyTransferApplication extends Application<MoneyTransferConfiguration> {
 
@@ -31,7 +36,7 @@ public class MoneyTransferApplication extends Application<MoneyTransferConfigura
     @Override
     public void run(final MoneyTransferConfiguration configuration,
                     final Environment environment) {
-
+        configureCors(environment);
         TransferDAO transferDAO = new TransferDAO(hibernate.getSessionFactory());
         AccountDAO accountDAO = new AccountDAO(hibernate.getSessionFactory());
         JerseyEnvironment jersey = environment.jersey();
@@ -41,6 +46,20 @@ public class MoneyTransferApplication extends Application<MoneyTransferConfigura
         jersey.register(accountResource);
         jersey.register(new BusinessExceptionMapper());
 
+    }
+
+    private void configureCors(Environment environment) {
+        // Enable CORS headers
+        final FilterRegistration.Dynamic cors =
+                environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+        // Configure CORS parameters
+        cors.setInitParameter("allowedOrigins", "*");
+        cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin");
+        cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+
+        // Add URL mapping
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
     }
 
     private final HibernateBundle<MoneyTransferConfiguration> hibernate = new HibernateBundle<MoneyTransferConfiguration>(Transfer.class, Account.class) {
